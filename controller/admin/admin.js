c1fd961e127089d2ec1a6490acf42a12fe1ebb49
Huynh Huy <bhanhmay31@gmail.com>
2020-08-23 17:36:33 +0700

    Func Admin ctrl,views,route

--- controller/admin/admin.js
+++ controller/admin/admin.js
@@ -3,15 +3,14 @@ const getListUser=(req,res)=>{
     let sql="SELECT list.*, kh.isDelete from (SELECT ttkh.TenDangNhap, ttkh.HoTen, ttkh.DiaChi,ttkh.SoDienThoai from thongtinkhachhang ttkh UNION select ct.TenDangNhap,ct.HoTen,ct.DiaChi, ct.SoDienThoai from chutro ct) list INNER JOIN khachhang kh on list.TenDangNhap=kh.TenDangNhap ";
     connectMySQL.query(sql,(err,results,feild)=>{
         if(err) return err;
-        console.log(results);
-        res.json(results);  
+        res.render("admin", {user: results, hotel: null });  
     });
 };
 const deleteUser=(req,res)=>{
-    let tenDangNhap=req.body;
+    let tenDangNhap=req.params.id;
     let sql="update khachhang set isDelete=true where TenDangNhap=? ";
     connectMySQL.query(sql,tenDangNhap,(err,results)=>{
-        if(err)return err;
+        if(err){ return err;}
         res.status(200).json({message:"Delete sucess"});
     })
 }
@@ -19,11 +18,11 @@ const getListHotel=(req,res)=>{
     let sql="select pt.*,ct.HoTen HoTenChuTro from phongtro pt inner join chutro ct on pt.IdChuTro=ct.TenDangNhap";
     connectMySQL.query(sql,(err,results,feild)=>{
         if(err)return err;
-        res.json(results);
+        res.render("admin", {hotel: results, user: null})
     })
 }
 const deleteHotel=(req,res)=>{
-    let id=req.body;
+    let id=req.params.id;
     let sql="update phongtro set isDelete=true where Id=?";
     connectMySQL.query(sql,id,(err,results)=>{
         if(err)return err;
--- routes/admin.js
+++ routes/admin.js
@@ -9,4 +9,10 @@ router.get('/listUser',(req,res)=>{
 router.get('/listHotel',(req,res)=>{
     admin.getListHotel(req,res);
 });
+router.get('/deleteUser/:id',(req,res)=>{
+    admin.deleteUser(req,res);
+})
+router.get('/deleteHotel/:id',(req,res)=>{
+    admin.deleteHotel(req,res);
+})
 module.exports = router;
